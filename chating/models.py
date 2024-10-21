eab57b16c9fc1e431fd886ef92e08301a1a92f41
ghostclog <kdy11281@gmail.com>
2024-10-21 14:36:44 +0900

    임베딩용 컬럼 추가

--- chating/models.py
+++ chating/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+import json
 
-# Create your models here.
 class ChatingRoom(models.Model):
     user = models.ForeignKey(
-        "user.User", 
+        "user.User",
         on_delete=models.CASCADE,
         related_name="ChatingRoom"
     )
@@ -12,12 +12,18 @@ class ChatingRoom(models.Model):
         default="gpt-4o-mini",
         max_length=30
     )
+    pdf_embedding = models.JSONField(null=True, blank=True)  # JSONField 사용 / 실제 저장은 문자열로
+
+    def save_pdf_embedding(self, embedding):
+        self.pdf_embedding = json.dumps(embedding)  # 임베딩을 JSON으로 저장
+        self.save()
+
 
 class Chating(models.Model):
     chatingRoom = models.ForeignKey(
         ChatingRoom,
         on_delete=models.CASCADE,
-        related_name="Chating",
+        related_name="chating",
     )
     chat = models.TextField()
     speaker = models.CharField(max_length=30)
